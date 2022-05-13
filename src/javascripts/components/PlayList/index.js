dc7f458c752bd90d9679743d7363a5ebfaa40273
MyeongHoonNam <codenmh0822@naver.com>
2022-05-13 17:21:47 +0900

    feat: view render 함수 구현

---
+++ src/javascripts/components/PlayList/index.js
@@ -0,0 +1,48 @@
+export default class PlayList {
+  constructor() {
+    this.rootElement = PlayList.createRootElement();
+    this.musicList = [];
+  }
+
+  static createRootElement() {
+    const rootElement = document.createElement("article");
+    rootElement.classList.add("contents-playlist");
+
+    return rootElement;
+  }
+
+  render() {
+    const playListTitle = `<h2 class="playlist-title">My PlayList</h2>`;
+    const musicList = this.musicList
+      .map((music) => {
+        const { cover, title, artists } = music;
+
+        return `
+        <li>
+          <div class="music-content">
+            <div class="music-data">
+              <div class="music-cover">
+                <img src=${cover}/>
+              </div>
+              <div class="music-info">
+                <strong class="music-title">${title}</strong>
+                <em class="music-artist">${artists[0]}</em>
+              </div>
+            </div>
+            <div class="music-controller">
+              <button class="icon icon-minus">
+                <span class="invisible-text">제거</span>
+              </button>
+            </div>
+          </div>
+        </li>
+      `;
+      })
+      .join("");
+
+    this.rootElement.innerHTML =
+      playListTitle + '<ul class="music-list">' + musicList + "</ul>";
+
+    return this.rootElement;
+  }
+}
